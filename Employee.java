8db3c8f2dfa7ac53ef2370d8880d37dbabe44f6a
tsvetomiraD <95402102+tsvetomiraD@users.noreply.github.com>
2022-12-02 16:50:42 +0200

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,15 @@
+import java.math.BigDecimal;
+import java.sql.Date;
+
+public class Employee {
+	public Integer id;
+    public String firstName;
+    public String lastName;
+    public String email;
+    public String phone;
+    public Date hireDate;
+    public int jobId;
+    public BigDecimal salary;
+    public int managerId;
+    public int departmentId;
+}
